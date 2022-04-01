6a2bf7348d61d610a15623a5e1e86996b8237172
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-04-01 22:36:19 +0200

    Fixed bugs in file VariablesTheme

--- Lesson1/VariablesTheme.java
+++ Lesson1/VariablesTheme.java
@@ -16,29 +16,27 @@ public class VariablesTheme {
 
         // Calculation of the cost of goods with a discount
         System.out.println("\nCalculation of the cost of goods with a discount:");
-        int productX = 100;
-        int productY = 200;
-        float discount = (productX + productY) * 0.11f;
-        float sumWithDiscount = productX + productY - discount;
-        System.out.println("Price with discount = " + (productX + productY - ((productX + productY) * 11 / 100))
-                + "\nDiscount amount = " + ((productX + productY) * 11 / 100));
+        int price1 = 100;
+        int price2 = 200;
+        System.out.println("Discount amount = " + ((price1 + price2) * 11 / 100));
+        System.out.println("Sum of price with discount = " + (price1 + price2 - ((price1 + price2) * 11 / 100)));
 
         // Output to the console word JAVA
         System.out.println("\nOutput to the console word JAVA:");
-        System.out.println("   J    a  v     v  a   "
-                + "\n   J   a a  v   v  a a  "
-                + "\nJ  J  aaaaa  V V  aaaaa "
-                + "\n JJ  a     a  V  a     a");
+        System.out.println("   J    a  v     v  a   ");
+        System.out.println("   J   a a  v   v  a a  ");
+        System.out.println("J  J  aaaaa  V V  aaaaa ");
+        System.out.println(" JJ  a     a  V  a     a");
 
         // Display min and max values of numeric data types
-        System.out.println("Display min and max values of numeric data types:");
+        System.out.println("\nDisplay min and max values of numeric data types:");
         byte b = 127;
         short s = 32_767;
         int i = 2_147_483_647;
         long ln = 9_223_372_036_854_775_807l;
-        System.out.println("byte max value = " + b + " short max value = " + s + " int max value = " + i + " long max value = " + ln
-                + "\nIncrement:" + " byte = " + ++b + " short = " + ++s + " int = " + ++i + " long  = " + ++ln
-                + "\nDecrement:" + " byte = " + --b + " short = " + --s + " int = " + --i + " long  = " + --ln);
+        System.out.println("byte max value = " + b + " short max value = " + s + " int max value = " + i + " long max value = " + ln);
+        System.out.println("Increment:" + " byte = " + ++b + " short = " + ++s + " int = " + ++i + " long  = " + ++ln);
+        System.out.println("Decrement:" + " byte = " + --b + " short = " + --s + " int = " + --i + " long  = " + --ln);
 
         // Rearranging variable values
         System.out.println("\nRearranging Variable Values:");
@@ -52,56 +50,56 @@ public class VariablesTheme {
 
         // Output of characters and their codes
         System.out.println("\nOutput of characters and their codes:");
-        int NumberOfCharOne = 35;
-        int NumberOfCharTwo = 38;
-        int NumberOfCharThree = 64;
-        int NumberOfCharFour = 94;
-        int NumberOfCharFifth = 95;
-        System.out.println(NumberOfCharOne + " = " + (char) NumberOfCharOne
-                + "\n" + NumberOfCharTwo + " = " + (char) NumberOfCharTwo
-                + "\n" + NumberOfCharThree + " = " + (char) NumberOfCharThree
-                + "\n" + NumberOfCharFour + " = " + (char) NumberOfCharFour
-                + "\n" + NumberOfCharFifth + " = " + (char) NumberOfCharFifth);
+        int numberChar1 = 35;
+        int numberChar2 = 38;
+        int numberChar3 = 64;
+        int numberChar4 = 94;
+        int numberChar5 = 95;
+        System.out.println(numberChar1 + " = " + (char) numberChar1);
+        System.out.println(numberChar2 + " = " + (char) numberChar2);
+        System.out.println(numberChar3 + " = " + (char) numberChar3);
+        System.out.println(numberChar4 + " = " + (char) numberChar4);
+        System.out.println(numberChar5 + " = " + (char) numberChar5);
 
         // Product and sum of digits of a number
         System.out.println("\nProduct and sum of digits of a number:");
-        int number = 345;
-        int numberC = number % 10;
-        number = number / 10;
-        int numberB = number % 10;
-        int numberA = number / 10;
-        System.out.println("Product of numbers 345 = " + (numberA * numberB * numberC)
-                + "\nSum of numbers 345 = " + (numberA + numberB + numberC));
+        int srcNumber = 345;
+        int units = srcNumber % 10;
+        srcNumber /= 10;
+        int tens = srcNumber % 10;
+        int hundreds = srcNumber / 10;
+        System.out.println("Product of numbers 345 = " + (hundreds * tens * units));
+        System.out.println("Sum of numbers 345 = " + (hundreds + tens + units));
 
         // Duke's ASCII art console output
         System.out.println("\nDuke's ASCII art console output:");
         char symbolSpace = ' ';
         char symbolSlash = '/';
-        char symbolBackSlash = 92;
+        char symbolBackSlash = '\\';
         char symbolUnderscore = '_';
         char symbolLeftParentheses = '(';
         char symbolRightParentheses = ')';
-        System.out.println("\n" + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolSlash + symbolBackSlash
-                + "\n" + symbolSpace + symbolSpace + symbolSpace + symbolSlash + symbolSpace + symbolSpace + symbolBackSlash
-                + "\n" + symbolSpace + symbolSpace + symbolSlash + symbolUnderscore + symbolLeftParentheses + symbolSpace + symbolRightParentheses + symbolBackSlash
-                + "\n" + symbolSpace + symbolSlash + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolBackSlash
-                + "\n" + symbolSlash + symbolUnderscore + symbolUnderscore + symbolUnderscore + symbolUnderscore + symbolSlash + symbolBackSlash
+        System.out.println("" + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolSlash + symbolBackSlash);
+        System.out.println("" + symbolSpace + symbolSpace + symbolSpace + symbolSlash + symbolSpace + symbolSpace + symbolBackSlash);
+        System.out.println("" + symbolSpace + symbolSpace + symbolSlash + symbolUnderscore + symbolLeftParentheses + symbolSpace + symbolRightParentheses + symbolBackSlash);
+        System.out.println("" + symbolSpace + symbolSlash + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolSpace + symbolBackSlash);
+        System.out.println("" + symbolSlash + symbolUnderscore + symbolUnderscore + symbolUnderscore + symbolUnderscore + symbolSlash + symbolBackSlash
                 + symbolUnderscore + symbolUnderscore + symbolBackSlash);
 
         // Displaying the number of hundreds, tens and units of a number
         System.out.println("\nDisplaying the number of hundreds, tens and units of a number:");
         int numberToDivide = 123;
-        System.out.println(numberToDivide + " Number for divide."
-                + "\n" + numberToDivide / 100 + " Divide by 100 to get hundreds."
-                + "\n" + numberToDivide / 10 % 10 + " For find number of tens: divide by 10 and modulo divide by 10."
-                + "\n" + numberToDivide % 10 + " For find number of units: modulo divide by 10.");
+        System.out.println(numberToDivide + " Number for divide.");
+        System.out.println(numberToDivide / 100 + " hundred. Divide by 100 to get hundreds.");
+        System.out.println(numberToDivide / 10 % 10 + " tens. For find number of tens: divide by 10 and modulo divide by 10.");
+        System.out.println(numberToDivide % 10 + " units. For find number of units: modulo divide by 10.");
 
         // Seconds conversion
         System.out.println("\nSeconds conversion:");
-        int numberOfSeconds = 86399;
-        int hoursInSeconds = numberOfSeconds / 3600;
-        int minutesInSeconds = numberOfSeconds % 3600 / 60;
-        int restOfSeconds = numberOfSeconds % 60;
-        System.out.println(hoursInSeconds + ":" +  minutesInSeconds + ":" +  restOfSeconds);
+        int srcSeconds = 86399;
+        int hours = srcSeconds / 3600;
+        int minutes = srcSeconds % 3600 / 60;
+        int seconds = srcSeconds % 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
     }
 }
