4fc5b0ae30dc09e6f8ca774d897b1df88621531b
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-05-14 10:58:12 +0200

    Add package lesson_5 and files create_db.sql, init_db.sql, queries.sql.

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,14 @@
+-- init database
+
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+    VALUES
+        ('Brawler Yukon',    'Mark-1',  '77.72',  '1.980',  'Destroyed', 'United States of America',  'January 30, 2015',    '2'),
+        ('Horizon Brave',    'Mark-1',  '72.54',  '7.890',  'Destroyed', 'China',                     'December 22, 2015',   '2'),
+        ('Romeo Blue',       'Mark-1',  '77.72',  '7.775',  'Alive',     'United States of America',  'December 15, 2015',   '2'),
+        ('Gipsy Danger',     'Mark-3',  '79.25',  '1.980',  'Destroyed', 'United States of America',  'July 10, 2017',       '9'),
+        ('Crimson Typhoon',  'Mark-4',  '76.2',   '1.722',  'Alive',     'China',                     'August 22, 2018',     '7'),
+        ('Striker Eureka',   'Mark-5',  '76.2',   '1.850',  'Destroyed', 'Australia',                 'November 2, 2019',    '11'),
+        ('Tacit Ronin',      'Mark-1',  '74.37',  '7.450',  'Alive',     'Japan',                     'December 6, 2015',    '3'),
+        ('Cherno Alpha',     'Mark-1',  '85.34',  '2.412',  'Alive',     'Russia',                    'December 15, 2015',   '6'),
+        ('Matador Fury',     'Mark-3',  '79.25',  '1.850',  'Alive',     'Mexico',                    'September 14, 2017',  '2'),
+        ('Vulcan Specter',   'Mark-3',  '72.54',  '2.412',  'Destroyed', 'Australia',                 'July 10, 2017',       '5');
